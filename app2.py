35f7fc589abc6d846af5296a9264be430067bfd7
Surajit Giri <girisura6@gmail.com>
2026-04-25 11:32:29 +0530

    Add files via upload

--- app2.py
+++ app2.py
@@ -2,23 +2,18 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+jobs = []
+
 @app.route("/")
 def home():
-    return "HireFlow API Running"
+    return "API Running"
 
 @app.route("/add-job", methods=["POST"])
 def add_job():
     data = request.get_json()
-    return jsonify({"message": "Job added successfully"})
+    jobs.append(data)
+    return {"message": "Job added"}
 
 @app.route("/get-jobs", methods=["GET"])
 def get_jobs():
-    return jsonify([])
-
-@app.route("/analyze-resume", methods=["POST"])
-def analyze_resume():
-    data = request.get_json()
-    return jsonify({"result": "Resume looks good!"})
-
-if __name__ == "__main__":
-    app.run()
+    return jsonify(jobs)
